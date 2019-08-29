4a52e8a0f0bd55f14d0e4991fcfd5474d93a5756
iK4tsu <joao.m.lourenco@hotmail.com>
2019-08-29 12:19:30 +0100

    SystemFactory now communicates only with Hub from ecs, and the user's systems

--- source/example/systemFactory.d
+++ source/example/systemFactory.d
@@ -1,17 +1,17 @@
 module example.systemFactory;
 
-import ecs.system;
-import ecs.isystem;
 import ecs.hub;
 
+// import your systems
+import example.systems.movement;
 
 void generateSystems(ref Hub hub)
 {
 	/*
-	 * generate all of your systems;
+	 * generate all of your systems through the hub;
 	 * the order you add your systems is the order they will get updated
 	 * so the systems who needs to be updated first should be added in the first position
 	 * for example, the render system should be one the last being added
 	 */ 
-	hub.systemCreate!MovementSystem;
+	hub.systemCreate!Movement;
 }
