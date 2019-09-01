a0ee5b178a297c4bf6eb7661a9912c814af8d55b
iK4tsu <joao.m.lourenco@hotmail.com>
2019-09-01 18:40:52 +0100

    Fixed typo

--- source/ecs/exceptions/entity/entityComponentIsNotDisabledException.d
+++ source/ecs/exceptions/entity/entityComponentIsNotDisabledException.d
@@ -7,6 +7,6 @@ class EntityComponentIsNotDisabledException : ECS_Exception
 {
 	public this(uint id, string message, string hint)
 	{
-		super(message ~ "\nEntity does not have a compontent with the id of " ~ to!(string)(id) ~ "disabled!", hint);
+		super(message ~ "\nEntity does not have a compontent with the id of " ~ to!(string)(id) ~ " disabled!", hint);
 	}
 }
--- source/ecs/exceptions/entity/entityDoesNotExistException.d
+++ source/ecs/exceptions/entity/entityDoesNotExistException.d
@@ -7,7 +7,7 @@ class EntityDoesNotExistException : ECS_Exception
 {
 	public this(uint eid, string message, string hint)
 	{
-		super(message ~ "\nThe entity with the id of " ~ to!(string)(id) ~
-		"does not exist! Maybe it was already killed or it was not created yet!", hint);
+		super(message ~ "\nThe entity with the id of " ~ to!(string)(eid) ~
+		" does not exist! Maybe it was already killed or it was not created yet!", hint);
 	}
 }
