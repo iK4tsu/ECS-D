5182b66b32e2c9798b3708a54c0c2b6e54b6b2c9
iK4tsu <joao.m.lourenco@hotmail.com>
2019-09-01 17:47:59 +0100

    Added an exception when trying to destroy a not existing entity

--- source/ecs/exceptions/entity/package.d
+++ source/ecs/exceptions/entity/package.d
@@ -4,3 +4,5 @@ public:
 	import ecs.exceptions.entity.entityDoesNotContainComponentException;
 	import ecs.exceptions.entity.entityAlreadyContainsComponentException;
 	import ecs.exceptions.entity.entityComponentIsNotDisabledException;
+
+	import ecs.exceptions.entity.entityDoesNotExistException;
