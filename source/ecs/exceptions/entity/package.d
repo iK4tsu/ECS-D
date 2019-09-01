312b96b0c659d7e814214cbdce9aa728da8d8f71
iK4tsu <joao.m.lourenco@hotmail.com>
2019-09-01 17:07:53 +0100

    Added an exception when trying to enable a component in an Entity which isn't disabled

--- source/ecs/exceptions/entity/package.d
+++ source/ecs/exceptions/entity/package.d
@@ -3,3 +3,4 @@ module ecs.exceptions.entity;
 public:
 	import ecs.exceptions.entity.entityDoesNotContainComponentException;
 	import ecs.exceptions.entity.entityAlreadyContainsComponentException;
+	import ecs.exceptions.entity.entityComponentIsNotDisabledException;
