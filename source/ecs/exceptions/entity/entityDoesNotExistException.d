17661a20151d58361935d22c104863e72bfb3d38
iK4tsu <joao.m.lourenco@hotmail.com>
2019-09-02 10:29:25 +0100

    Fixed typo

--- source/ecs/exceptions/entity/entityDoesNotExistException.d
+++ source/ecs/exceptions/entity/entityDoesNotExistException.d
@@ -8,6 +8,6 @@ class EntityDoesNotExistException : ECS_Exception
 	public this(uint eid, string message, string hint)
 	{
 		super(message ~ "\nThe entity with the id of " ~ to!(string)(eid) ~
-		" does not exist! Maybe it was already killed or it was not created yet!", hint);
+		" does not exist! Maybe it was already killed or it was not yet created!", hint);
 	}
 }
