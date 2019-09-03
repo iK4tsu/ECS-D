4a3384bce8b3cf983851ad99e410d44b7e5aef24
iK4tsu <joao.m.lourenco@hotmail.com>
2019-09-03 16:01:56 +0100

    removed all functions but update

--- source/ecs/hub.d
+++ source/ecs/hub.d
@@ -32,181 +32,14 @@ final class Hub
 	}
 
 
-	/********************************************* ENTITY MANAGER FUNCTIONS *********************************************/
-	public T entityAddComponent(T)(EntityId eid)
-	{
-		return componentExists!T ? entity.addComponent!(T)(eid, componentGetTypeId!T) : null;
-	}
-
-	public T entityAddComponent(T)(T t, EntityId eid)
-	{
-		return componentExists!T ? entity.addComponent!(T)(eid, t, componentGetTypeId!T) : null;
-	}
-
-	public void entityRemoveComponent(EntityId eid, ComponentTypeId id)
-	{
-		entity.removeComponent(eid, id);
-	}
-
-	public T entityGetComponent(T)(EntityId id)
-	{
-		return entity.getComponent!T(id);
-	}
-
-	//public Entity entityCreate(const string name, const EntityType type)
-	//{
-	//	EntityId id = entity.createEntity(name, type);
-	//	system.addEid(id);
-	//	return id;
-	//}
-
-	public void entityKill(EntityId id)
-	{
-		//system.removeEid(id);
-		entity.killEntity(id);
-	}
-
-	public Entity entityGetEntity(EntityId id)
-	{
-		return entity.getEntity(id);
-	}
-
-	public Entity entityGetEntity(EntityType type)
-	{
-		return entity.getEntity(type);
-	}
-
-	public void entityEnableComponent(EntityId eid, ComponentTypeId id)
-	{
-		entity.enableComponent(eid, id);
-	}
-
-	public void entityDisableComponent(EntityId eid, ComponentTypeId id)
-	{
-		entity.disableComponent(eid, id);
-	}
-
-	public bool entityIsComponentDisabled(EntityId eid, ComponentTypeId id)
-	{
-		return entity.isComponentDisabled(eid, id);
-	}
-
-	public bool entityHasComponents(EntityId eid, ComponentTypeId[] ids)
-	{
-		return entity.hasComponents(eid, ids);
-	}
-
-	public bool entityHasAnyComponent(EntityId eid, ComponentTypeId[] ids)
-	{
-		return entity.hasAnyComponent(eid, ids);
-	}
-
-	public bool entityHasComponent(EntityId eid, ComponentTypeId id)
-	{
-		return entity.hasComponent(eid, id);
-	}
-
-	public string entityGetName(EntityId eid)
-	{
-		return entity.getName(eid);
-	}
-
-	public string entityGetDescription(EntityId eid)
-	{
-		return entity.getDescription(eid);
-	}
-
-	public EntityType entityGetType(EntityId eid)
-	{
-		return entity.getType(eid);
-	}
-
-	public void entitySetDescription(EntityId eid, const string description)
-	{
-		entity.setDescription(eid, description);
-	}
-
-	public EntityId[] entityGetDeleted()
-	{
-		return entity.getDeletedEntities;
-	}
-
-	public EntityId entityGetId(Entity e)
-	{
-		return entity.getEntityId(e);
-	}
-
-	public EntityId entityGetId(EntityType type)
-	{
-		return entity.getEntityId(type);
-	}
-
-	public bool entityTypeExists(EntityType type)
-	{
-		return entity.typeExists(type);
-	}
-
-	public bool entityExists(EntityId eid)
-	{
-		return entity.hasEntity(eid);
-	}
-
-
-	/********************************************* COMPONENT MANAGER FUNCTIONS *********************************************/
-	public ComponentTypeId componentCreate(T)()
-	{
-		return component.createComponent!T;
-	}
-
-	public bool componentExists(T)()
-	{
-		return component.hasComponent!T;
-	}
-
-	public T componentGet(T)()
-	{
-		return component.getComponent!T;
-	}
-
-	public ComponentName componentGetName(ComponentTypeId id)
-	{
-		return component.getComponentName(id);
-	}
-
-	public ComponentName componentGetName(T)()
-	{
-		return component.getComponentName!T;
-	}
-
-	public ComponentTypeId componentGetTypeId(T)()
-	{
-		return component.getComponentTypeId!T;
-	}
-
-
-	/*************************************************** SYSTEM FUNCTIONS **************************************************/
-	public void updateSystems()
+	public void update()
 	{
 		system.update;
-	}
-
-	public void systemCreate(T)()
-	{
-		system.createSystem!T;
-	}
-
-	public T systemGet(T)()
-	{
-		return system.getSystem!T;
-	}
-
-	public bool systemExists(T)()
-	{
-		return system.existsSystem!T;
 	}
 }
 
 
+/*
 @system unittest
 {
 	Hub _hub = new Hub();
@@ -231,4 +64,4 @@ final class Hub
 	e.enableComponent(fooID);
 
 	assert(e.getComponent!Foo.someData == 1);
-}
+}*/
