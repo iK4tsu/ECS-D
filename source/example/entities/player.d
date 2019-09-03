7f4ae5c5912e057ca7c5fd69f98d3eb72d08cdd9
iK4tsu <joao.m.lourenco@hotmail.com>
2019-09-03 14:49:21 +0100

    Fixed player creation

--- source/example/entities/player.d
+++ source/example/entities/player.d
@@ -7,12 +7,12 @@ import example.components.position;
 import example.components.movable;
 
 
-public EntityId createPlayer(ref Hub hub)
+public Entity createPlayer(ref Hub hub)
 {
-	EntityId eid = hub.entityCreate("Player", "Player");
+	Entity e = hub.entity.createEntity("Player", "Player");
 
-	Position position = hub.entityAddComponent!Position(eid);
-	Movable movable = hub.entityAddComponent(new Movable(4) , eid);
+	Position position = e.addComponent!Position;
+	Movable movable = e.addComponent(new Movable(4));
 
-	return eid; 
+	return e; 
 }
